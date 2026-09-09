8abeade95010ce88c5bd1e1f4e650cbd72721dc9
legacycore2-lab <legacycore2@gmail.com>
2026-09-09 17:42:11 +0300

    refactor: use @/lib/fmt

--- src/pages/reports/tabs/AlertsTab.tsx
+++ src/pages/reports/tabs/AlertsTab.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 // src/pages/reports/tabs/AlertsTab.tsx
 import { cn } from '@/lib/cn'
-import { fmt } from '@/constants/statusMaps'
+import { fmt } from '@/lib/fmt'
 import { Badge } from '@/components/ui/Badge'
 import { KpiCard, BarChart } from '../components/ReportWidgets'
 import {
